fa8eb098f79e89e4b00d836a8ab766a8d8d9cd70
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-17 19:24:37 +0800

    Add Camera Positions and Done !

--- Unity Projects/Roll A Ball/Assets/Scripts/cameraController.cs
+++ Unity Projects/Roll A Ball/Assets/Scripts/cameraController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour {
 
+    public GameObject player;
+
+    // Offset is private because we can set the value HERE IN SCRIPT
+    private Vector3 offset;
+
 	// Use this for initialization
 	void Start () {
-		
+        offset = transform.position - player.transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// LateUpdate is guaranteed to run AFTER all items 
+    // have been processed in update
+    // Once set the camera , we will absolutely know the player that moved
+    // for that frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
 	}
 }
