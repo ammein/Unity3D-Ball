bd8650aeda19bac3f5f5b7db8bb5c198a131df8d
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-20 14:07:50 +0800

    Done Play the game. Later Build It...

--- Unity Projects/Roll A Ball/Assets/Scripts/playerController.cs
+++ Unity Projects/Roll A Ball/Assets/Scripts/playerController.cs
@@ -1,18 +1,23 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.UI; // Unity Namespace UI
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class playerController : MonoBehaviour {
     // apply input to player 
     public float speed;
     private Rigidbody rb;
     private int count; // Declare private value
+    public Text countText; // Init text variable
+    public Text winText; // Init Total Text
 
     void Start()
     {
         // Get Component of Rigidbody in Unity Object
         rb = GetComponent<Rigidbody>();
         count = 0; // Initialize Count
+        SetCountText(); // Initialize Count Text
+        winText.text = ""; // Initialize Win Text
     }
 
     void FixedUpdate()
@@ -36,6 +41,17 @@ public class playerController : MonoBehaviour {
         {
             other.gameObject.SetActive(false); // Just like a checkbox to activate/deactivate
             count++; // Add count by 1
+            SetCountText(); // Initialize Count Text
+        }
+    }
+
+    // Create a new function
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString(); // Initialize Count Text
+        if(count >= 9)
+        {
+            winText.text = "You Win !"; // Set Win Text
         }
     }
 
