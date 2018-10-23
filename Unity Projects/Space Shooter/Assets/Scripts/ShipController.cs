7f5c8bd2ce634144ba89698964b0d50158beadff
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-24 02:26:55 +0800

    Done Moving Player

--- Unity Projects/Space Shooter/Assets/Scripts/ShipController.cs
+++ Unity Projects/Space Shooter/Assets/Scripts/ShipController.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+// Reusable code for Min & Max
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
 
 public class ShipController : MonoBehaviour {
     private Rigidbody rb;
     public float speed;
+    public float tilt;
+
+    // Init Instance of Boundary class
+    public Boundary boundary;
 
     void Start()
     {
         // To get current gameObject (Ship) RigidBody
-        rb = GetComponent<Rigidbody>();    
+        rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
@@ -22,5 +32,15 @@ public class ShipController : MonoBehaviour {
 
         // Access Rigidbody Velocity (Physics Access)
         rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                0.0f, 
+                Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            ); // Set Rigidbody's position
+
+        // Tilt Code
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * tilt);
     }
 }
