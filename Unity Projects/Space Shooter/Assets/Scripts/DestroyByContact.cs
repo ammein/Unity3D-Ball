97d50a213a0ed8abe6c0dbfa81a181aef2dfcef2
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-26 15:04:28 +0800

    Asteroid Done

--- Unity Projects/Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Unity Projects/Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -6,6 +6,11 @@ public class DestroyByContact : MonoBehaviour {
     // Destroy everything that enters the trigger
     void OnTriggerEnter(Collider other)
     {
+        // Debugging
+        if(other.tag == "Boundary")
+        {
+            return; //  Return null OR end this function at this point
+        }
         Destroy(other.gameObject);
         Destroy(gameObject); // Destroy Asteroid Itself to all its children and to all its component
     }
