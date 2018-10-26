b9568b98c3cfe903307e2fd2e7b19aa587df3149
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-26 12:54:22 +0800

    Asteroid missing

---
+++ Unity Projects/Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+    // Destroy everything that enters the trigger
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject); // Destroy Asteroid Itself to all its children and to all its component
+    }
+}
