de6b94d06595de5e7cc538be27e07dcf4d6934e7
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-26 18:59:48 +0800

    Done GameController !

---
+++ Unity Projects/Space Shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour {
+    public float lifetime; // To make a wait
+
+	// Use this for initialization
+	void Start () {
+        // Simple code when initiate , immediatly destroy the gameObject
+        Destroy(gameObject , lifetime);
+	}
+}
--- Unity Projects/Space Shooter/Assets/Scripts/GameController.cs
+++ Unity Projects/Space Shooter/Assets/Scripts/GameController.cs
@@ -2,20 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
     public GameObject hazard;
     public Vector3 spawnValues; // use another variable
+    public int hazardCount; // To define how many hazard to spawn
+    public float spawnWait; // To hold before spawning the next one
+    public float startWait;
+    public float waveWait;
 
     void Start()
     {
-        SpawnWaves(); // Initialize
+        StartCoroutine(SpawnWaves()); // Initialize
     }
 
-    void SpawnWaves()
+    // To make enable for WaitForSeconds
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPosition = new Vector3();
-        Quaternion spawnRotation = new Quaternion();
-        // We want to instantiate our hazards
-        Instantiate(hazard, spawnPosition, spawnRotation); // These are the function that we are going to declare later on
+        yield return new WaitForSeconds(spawnWait); // To start wait before initialize for loop
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity; // Just read the rotation value
+                                                                // We want to instantiate our hazards
+                Instantiate(hazard, spawnPosition, spawnRotation); // These are the function that we are going to declare later on
+                yield return new WaitForSeconds(spawnWait); // To wait before initialize the next loop
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
