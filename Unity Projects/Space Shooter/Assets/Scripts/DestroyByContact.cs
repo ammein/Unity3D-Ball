6452af8f197323287638a0adcba3c3cc14671e57
Amin Shazrin <aminshazrin@yahoo.com>
2018-10-26 17:09:17 +0800

    Later sambung

--- Unity Projects/Space Shooter/Assets/Scripts/DestroyByContact.cs
+++ Unity Projects/Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
+    public GameObject explosion; // Add Reference
+    public GameObject playerExplosion; // Add Reference to our PlayerShip
+
     // Destroy everything that enters the trigger
     void OnTriggerEnter(Collider other)
     {
@@ -10,6 +13,12 @@ public class DestroyByContact : MonoBehaviour {
         if(other.tag == "Boundary")
         {
             return; //  Return null OR end this function at this point
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation); // Instantiate our explosions
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation); // Instantiate our explosions
         }
         Destroy(other.gameObject);
         Destroy(gameObject); // Destroy Asteroid Itself to all its children and to all its component
---
+++ Unity Projects/Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject hazard;
+    public Vector3 spawnValues; // use another variable
+
+    void Start()
+    {
+        SpawnWaves(); // Initialize
+    }
+
+    void SpawnWaves()
+    {
+        Vector3 spawnPosition = new Vector3();
+        Quaternion spawnRotation = new Quaternion();
+        // We want to instantiate our hazards
+        Instantiate(hazard, spawnPosition, spawnRotation); // These are the function that we are going to declare later on
+    }
+}
